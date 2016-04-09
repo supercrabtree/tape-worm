ce0a5c2bdd32508c50e791d5deea96b18516938f
George Crabtree <supercrabtree@googlemail.com>
2016-04-09 07:30:05 +0200

    Add test to check the favicon is changed

--- test/test.js
+++ test/test.js
@@ -3,6 +3,19 @@ var tapeworm = require('../');
 
 tapeworm.infect(test);
 
+test('infect should inject a custom favicon into the DOM', function (t) {
+  var favicon = document.querySelector('link[rel="icon"]');
+  var faviconHref = favicon.getAttribute('href');
+
+  t.equal(true, !!favicon, 'favicon element exists');
+  t.equal(
+    true,
+    /^data:image\/png;base64,/.test(faviconHref),
+    'favicon href is a base64 png'
+  );
+  t.end();
+});
+
 test('infect should add a method html to t', function (t) {
   t.equal(typeof t.html, 'function');
   t.end();
